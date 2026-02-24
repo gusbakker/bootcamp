ca9e7963aca880a2633c241fc3ade061d003c932
Gustavo Bakker <gustavobakker@hotmail.com>
2026-02-24 23:18:44 +0100

    feat: modernize templates with Bootstrap updates and improved accessibility styles

--- config/settings/base.py
+++ config/settings/base.py
@@ -92,6 +92,7 @@ LOCAL_APPS = [
     "bootcamp.notifications.apps.NotificationsConfig",
     "bootcamp.search.apps.SearchConfig",
     "bootcamp.groups.apps.GroupsConfig",
+    # "bootcamp.ai.apps.AIConfig",
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
